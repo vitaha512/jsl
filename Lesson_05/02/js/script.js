170bc3908fefe815c338224fa313ca4c996da4b7
Test <test@test.com>
2018-08-22 13:42:21 +0300

    Выполнил урок №5, задание №2

---
+++ Lesson_05/02/js/script.js
@@ -0,0 +1,25 @@
+//1
+let openBtn = document.getElementById("open-btn");
+
+//2
+let nameValue = document.getElementsByClassName("name-value");
+let budgetValue = document.getElementsByClassName("budget-value");
+let goodsValue = document.getElementsByClassName("goods-value");
+let itemsValue = document.getElementsByClassName("items-value");
+let employersValue = document.getElementsByClassName("employers-value");
+let discountValue = document.getElementsByClassName("discount-value");
+let isopenValue = document.getElementsByClassName("isopen-value");
+
+//3
+let goodsItem = document.getElementsByClassName("goods-item");
+
+//4
+let button = document.getElementsByTagName("button");
+
+//5
+let chooseItem = document.querySelector(".choose-item");
+let timeValue = document.querySelector(".time-value");
+let countBudgetValue = document.querySelector(".count-budget-value");
+
+//6
+let hireEmployersItem = document.querySelectorAll(".hire-employers-item");
